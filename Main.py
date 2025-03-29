1f65ab0551a05cc3948243b47ab83c79f528b12b
ITT/2021/003.Sadev hirudithamed <sadevonline@gmailcom>
2025-03-30 00:26:23 +0530

    2nd

--- Main.py
+++ Main.py
@@ -2,3 +2,13 @@ import os
 import tkinter as tk
 from tkinter import filedialog, messagebox
 from PIL import Image
+
+class ImageRenamerApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Image Renamer")
+        self.root.geometry("500x300")
+        self.root.resizable(False, False)
+        
+        self.folder_path = ""
+        self.setup_ui()
